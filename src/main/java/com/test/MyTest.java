6e3ccb7e7a09a383ba6ba8acac47fae6443afb3e
zhangsan <zhangsan@163.com>
2021-01-12 19:22:27 +0800

    第八次修改testMoudules

--- src/main/java/com/test/MyTest.java
+++ src/main/java/com/test/MyTest.java
@@ -10,5 +10,7 @@ public class MyTest {
         System.out.println("edit-git first");
         System.out.println("第五次修改");
         System.out.println("第六次修改");
+        System.out.println("第七次修改");
+        System.out.println("第八次修改");
     }
 }
